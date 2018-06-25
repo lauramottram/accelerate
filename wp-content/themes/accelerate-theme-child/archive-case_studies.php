0d4fd200bf159c9fac7b8b77023e84fb06fb7072
Laura Mottram <lauramottram9@gmail.com>
2018-06-25 14:36:03 -0700

    Case studies, without large files.

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -12,11 +12,29 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-        <p>is this working?</p>
-				<?php the_content(); ?>
+<div id="primary" class="site-content">
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post();
+			$image_1 = get_field("image_1");
+			$size = "full";
+			$services = get_field('services');
+		?>
+
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2>
+			<h4><?php echo $services; ?></h4>
+			<?php the_excerpt(); ?>
+			<p class="read-more-link"><a href="<?php the_permalink(); ?>">View Project></a></p>
+		</aside>
+
+		<div class="case-study-images">
+			<?php if($image_1){
+			echo wp_get_attachment_image( $image_1, $size );
+			}?>
+		</div>
+	</article>
+
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
